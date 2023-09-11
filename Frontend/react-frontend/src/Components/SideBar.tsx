e5a37f377632442cc5bcf9919c9fdb1c32b6bab8
Ethan Hall <ephallbusiness@gmail.com>
2023-09-11 03:05:54 -0400

    Nav section done

--- Frontend/react-frontend/src/Components/SideBar.tsx
+++ Frontend/react-frontend/src/Components/SideBar.tsx
@@ -12,17 +12,23 @@ function SideBar(){
         setCollapseLinks(!collapseLinks);
     }
 
-    return <ul>
-    <li><a href="#about">About</a></li>
-    <li><a href="#projects">Projects</a></li>
-    <li><a href="#contact">Contact</a></li>
-    <li>
-        <button onClick={onLinksButtonClicked}>
-            {collapseLinks ? linksButtonCollapsedText : linksButtonOpenText}
-        </button>
-    </li>
-  </ul>
-
+    return <ul className="nav-list">
+        <li><a href="#about">About</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li>
+            <button onClick={onLinksButtonClicked}>
+                {collapseLinks ? linksButtonCollapsedText : linksButtonOpenText}
+            </button>
+            {collapseLinks ? <></> : 
+                <ul className="links-list">
+                    <li><a href="https://www.linkedin.com/in/ethan-hall-706224188/" target="_blank">LinkedIn</a></li>
+                    <li><a href="https://github.com/EthanPHall" target="_blank">GitHub</a></li>
+                    <li><a href="https://itch.io/profile/ephhall" target="_blank">Itch.io</a></li>
+                </ul>
+            }
+        </li>
+    </ul>
 }
 
 export default SideBar;
